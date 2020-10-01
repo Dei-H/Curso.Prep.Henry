6b4acc3b7647877347d89a18ce504e46a28fea9b
Dei H <davidhmiranda@gmail.com>
2020-10-01 01:39:45 -0300

    Finished homework - revision de codigos y modificacion

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -62,8 +62,8 @@ function sonIguales(x, y) {
   // Tu código:
   if (x === y) {
     return true;
-  } else {
-    return false;
+    } else {
+      return false;
   }
 }
 
@@ -170,8 +170,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  var combinado = nombre + ' ' + apellido;
-  return combinado;
+  return nombre + ' ' + apellido;
 }
 
 function obtenerSaludo(nombre) {
